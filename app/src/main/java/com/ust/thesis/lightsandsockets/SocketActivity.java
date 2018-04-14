ef23392b0154dc145aa464673b46fe5b714f535d
2014068627 <2014068627@ust-ics.mygbiz.com>
2018-04-14 11:55:40 +0800

    Add schedule activity

--- app/src/main/java/com/ust/thesis/lightsandsockets/SocketActivity.java
+++ app/src/main/java/com/ust/thesis/lightsandsockets/SocketActivity.java
@@ -1,20 +1,19 @@
 package com.ust.thesis.lightsandsockets;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-
-import java.util.ArrayList;
 
 public class SocketActivity extends AppCompatActivity {
 
@@ -23,6 +22,8 @@ public class SocketActivity extends AppCompatActivity {
     Button weeklyButton;
     Button dailyButton;
     Button showNV;
+    Button scheduleButton;
+    SwitchCompat s;
     private DailyGraphFragment DF;
     private WeeklyGraphFragment WF;
 
@@ -38,6 +39,8 @@ public class SocketActivity extends AppCompatActivity {
         socketNumber = (TextView) findViewById(R.id.socket_number);
         Iappliance = (TextView) findViewById(R.id.identifiedDevice);
         showNV = (Button) findViewById(R.id.showNV);
+        scheduleButton = (Button) findViewById(R.id.scheduleButton);
+        s = (SwitchCompat) findViewById(R.id.switch1);
         Bundle bundle = getIntent().getExtras();
         String socket = bundle.getString("socket"); /*Contains the name of the socket currently selected"*/
         String appliance = bundle.getString("appliance"); /*Temporary device identification*/
@@ -73,8 +76,11 @@ public class SocketActivity extends AppCompatActivity {
             }
         });
 
-        directToShowNV(showNV, socketNumber, Iappliance/*temporary*/);
 
+        EnableButton(s);
+        ChangeButtonlook(s.isChecked());
+        directToShowNV(showNV, socketNumber, Iappliance/*temporary*/);
+        directToSchedule(scheduleButton, socketNumber, Iappliance/*temporary*/);
     }
 
     private void setFragment(Fragment fragment){
@@ -98,4 +104,43 @@ public class SocketActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void directToSchedule(Button button, TextView number, TextView appliances/*temporary*/){
+        final String socket = number.getText().toString();
+        final String appliance = appliances.getText().toString();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(SocketActivity.this, ScheduleActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("socket", socket);
+                bundle.putString("appliance", appliance); /*temporary*/
+                myIntent.putExtras(bundle);
+                startActivity(myIntent);
+            }
+        });
+    }
+
+    public void EnableButton(SwitchCompat sw) {
+        scheduleButton = (Button) findViewById(R.id.scheduleButton);
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                ChangeButtonlook(isChecked);
+            }
+        });
+    }
+
+    public void ChangeButtonlook(boolean bol){
+        if(bol) {
+            scheduleButton.setBackgroundColor(Color.parseColor("#9c382d"));
+            scheduleButton.setTextColor(Color.parseColor("#dbc3c0"));
+            scheduleButton.setEnabled(true);
+        } else {
+            scheduleButton.setBackgroundColor(Color.parseColor("#dbc3c0"));
+            scheduleButton.setTextColor(Color.parseColor("#ffffff"));
+            scheduleButton.setEnabled(false);
+        }
+    }
+
 }
